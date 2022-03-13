9719c91a53f2b78d4d409e8adb998f911a7a722a
Moe Deera <deeramoe@gmail.com>
2022-03-13 09:06:01 -0600

    committ

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
 function App() {
+
+const fetchTest = async () => {
+  var id = 2
+ try {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    const string = `https://econnectsocial.herokuapp.com/api/users/profiles/${id}`;
+    console.log(string, id);
+    const res = await axios.get(string);
+    console.log(res.data);
+   
+  } catch (error) {
+    console.log(error);
+  }
+
+
+
+}
+ 
+fetchTest()
+
+
   return (
     <div className="App">
       <header className="App-header">
