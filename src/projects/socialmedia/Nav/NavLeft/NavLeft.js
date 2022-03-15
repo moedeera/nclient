898299b61aba34981f107d58e9cc6072faf9cc3d
moedeera <82620264+moedeera@users.noreply.github.com>
2022-03-15 15:06:28 -0600

    committ

--- src/projects/socialmedia/Nav/NavLeft/NavLeft.js
+++ src/projects/socialmedia/Nav/NavLeft/NavLeft.js
@@ -22,18 +22,113 @@ export const NavLeft = ({ status }) => {
           </div>
           <div className="nav-left-follow">
             {" "}
-            <h3>Following</h3>
+            <h4>Following</h4>
             <p>10</p>
           </div>
           <div className="nav-left-follow">
-            <h3>Followers</h3>
+            <h4>Followers</h4>
             <p>15</p>
           </div>
           <div className="nav-left-view">
             <h4>View Profile</h4>
           </div>
         </div>
-        <div className="nav-left-suggestions"></div>
+        <div className="nav-left-suggestions">
+          <div className="nav-left-header">
+            <h4>Suggestions</h4>
+            <i class="fas fa-ellipsis-v"></i>
+          </div>
+          <div className="nav-left-suggested-profile">
+            <div>
+              <img src={pic} alt="" />
+            </div>
+            <div className="nav-left-info">
+              {" "}
+              <h4>Jessica Swanson</h4>
+              <h5>Saskatoon,Sk</h5>
+            </div>
+            <div className="nav-left-add">
+              <i class="fas fa-plus"></i>
+            </div>
+          </div>
+
+          <div className="nav-left-suggested-profile">
+            <div>
+              <img src={pic} alt="" />
+            </div>
+            <div className="nav-left-info">
+              {" "}
+              <h4>David Smith</h4>
+              <h5>Weston,Sk</h5>
+            </div>
+            <div className="nav-left-add">
+              <i class="fas fa-plus"></i>
+            </div>
+          </div>
+
+          <div className="nav-left-suggested-profile">
+            <div>
+              <img src={pic} alt="" />
+            </div>
+            <div className="nav-left-info">
+              {" "}
+              <h4>Martin Lee</h4>
+              <h5>Madison,WI</h5>
+            </div>
+            <div className="nav-left-add">
+              <i class="fas fa-plus"></i>
+            </div>
+          </div>
+
+          <div className="nav-left-suggested-profile">
+            <div>
+              <img src={pic} alt="" />
+            </div>
+            <div className="nav-left-info">
+              {" "}
+              <h4>Amanda Busch</h4>
+              <h5>Edmonton,AB</h5>
+            </div>
+            <div className="nav-left-add">
+              <i class="fas fa-plus"></i>
+            </div>
+          </div>
+
+          <div className="nav-left-suggested-profile">
+            <div>
+              <img src={pic} alt="" />
+            </div>
+            <div className="nav-left-info">
+              {" "}
+              <h4>Larry Van Holt</h4>
+              <h5>Seattle,WSH</h5>
+            </div>
+            <div className="nav-left-add">
+              <i class="fas fa-plus"></i>
+            </div>
+          </div>
+
+          <div className="nav-left-suggested-profile">
+            <div>
+              <img src={pic} alt="" />
+            </div>
+            <div className="nav-left-info">
+              {" "}
+              <h4>Kent Holt</h4>
+              <h5>Seattle,WSH</h5>
+            </div>
+            <div className="nav-left-add">
+              <i class="fas fa-plus"></i>
+            </div>
+          </div>
+          <div className="nav-left-profile-image-colord"></div>
+          <div className="nav-left-dummy"></div>
+          <div className="md-suggestion-icons">
+            <i class="fas fa-cog fa-2x"></i>
+            <i class="fas fa-user-edit fa-2x"></i>
+            <i class="fas fa-comment fa-2x"></i>
+          </div>
+        </div>
       </div>
     );
   } else {
--- src/projects/socialmedia/Nav/NavRight/NavRight.js
+++ src/projects/socialmedia/Nav/NavRight/NavRight.js
@@ -1,27 +1,39 @@
 import React from "react";
+import "./NavRight.css";
+import pic from "./blank-avatar.png";
 
 export const NavRight = ({ status }) => {
   if (status) {
     return (
       <div className="nav-right">
-        <div className="searchbox">
-          <i class="fas fa-search"></i>
-          <input type="text" placeholder="search" />
+        <div className="nav-right-stats">
+          <div className="nav-right-stats-header">
+            <h3>Your Page</h3>
+            <div className="border"></div>
+          </div>
+          <div className="nav-right-stats-weekly">
+            <img src={pic} alt="" className="nav-right-img" />
+            <div className="nav-right-stats-weekly-info">
+              {" "}
+              <h4>Weekly overview </h4>
+              <h5>107 Visits</h5>
+              <h5>56 New Followers</h5>
+            </div>
+          </div>
+          <div className="nav-right-like-count">
+            <i class="far fa-heart fa-2x"> </i>107 Likes
+          </div>
+          <div>
+            <button className="btn btn-social-primary">View Stats</button>
+          </div>
         </div>
-
-        <div className="nav-user-icon"></div>
+        <div className="nav-right-ads">
+          <h3>Ad</h3>
+          <i class="fas fa-ellipsis-v"></i>
+        </div>
       </div>
     );
   } else {
-    return (
-      <div className="nav-right">
-        <div className="searchbox">
-          <i class="fas fa-search"></i>
-          <input type="text" placeholder="search" />
-        </div>
-
-        <div className="nav-user-icon"></div>
-      </div>
-    );
+    return <div className="nav-right"></div>;
   }
 };
