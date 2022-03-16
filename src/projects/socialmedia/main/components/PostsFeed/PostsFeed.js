1e75e44f8e5c22f5a0497137fc4a83e9cee43ba3
moedeera <82620264+moedeera@users.noreply.github.com>
2022-03-15 21:03:31 -0600

    committ

--- src/projects/socialmedia/main/components/PostsFeed/PostsFeed.js
+++ src/projects/socialmedia/main/components/PostsFeed/PostsFeed.js
@@ -72,40 +72,41 @@ export const PostsFeed = () => {
     <div className="Feed">
       <div className="Post-Feed">
         {Posts.map((Post) => (
-          <>
-            <div className="postFeed-post" key={Post.id}>
-              <div className="postFeed-post-info">
-                <img src={Post.PosterPic} alt="" />
-                <div className="postFeed-post-info-stamp">
-                  <h3>{Post.PosterName}</h3>
-                  <h5>Published {Post.date}</h5>
-                </div>
+          <div className="postFeed-post" key={Post.id}>
+            <div className="postFeed-post-info">
+              <img src={Post.PosterPic} alt="" />
+              <div className="postFeed-post-info-stamp">
+                <h3>{Post.PosterName}</h3>
+                <h5>Published {Post.date}</h5>
               </div>
-              <div className="postFeed-post-image">
-                {" "}
-                <img src={Post.postPic} alt="" />
-              </div>
-              <div className="postFeed-post-stats">
-                <div>
-                  <i class="fas fa-eye " style={{ color: "teal" }}></i>
-                  <small>12</small>
-                </div>
-                <div>
-                  <i class="far fa-heart" style={{ color: "crimson" }}></i>
-                  <small>{Post.likers.length}</small>
-                </div>
-                <div>
-                  <i class="fas fa-comment" style={{ color: "steelblue" }}></i>
-                  <small>{Post.comments.length}</small>
-                </div>
-                <div>
-                  <i class="fas fa-share" style={{ color: "navy" }}></i>
-                  <small>8</small>
-                </div>
-              </div>
-              <div className="postFeed-post-text">{Post.text}</div>
             </div>
-          </>
+            <div className="postFeed-post-image">
+              {" "}
+              <img src={Post.postPic} alt="" />
+            </div>
+            <div className="postFeed-post-stats">
+              <div>
+                <i className="fas fa-eye " style={{ color: "teal" }}></i>
+                <small>12</small>
+              </div>
+              <div>
+                <i className="far fa-heart" style={{ color: "crimson" }}></i>
+                <small>{Post.likers.length}</small>
+              </div>
+              <div>
+                <i
+                  className="fas fa-comment"
+                  style={{ color: "steelblue" }}
+                ></i>
+                <small>{Post.comments.length}</small>
+              </div>
+              <div>
+                <i className="fas fa-share" style={{ color: "navy" }}></i>
+                <small>8</small>
+              </div>
+            </div>
+            <div className="postFeed-post-text">{Post.text}</div>
+          </div>
         ))}
       </div>
     </div>
