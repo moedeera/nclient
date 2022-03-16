232a1b31be5acbdaf033ee4bcaef28b48d339b87
moedeera <82620264+moedeera@users.noreply.github.com>
2022-03-15 20:49:53 -0600

    committ

--- src/projects/socialmedia/main/components/PostsFeed/PostsFeed.js
+++ src/projects/socialmedia/main/components/PostsFeed/PostsFeed.js
@@ -57,9 +57,9 @@ export const PostsFeed = ({ LikeUpdate, CommentUpdate }) => {
         "https://images.pexels.com/photos/1693085/pexels-photo-1693085.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
 
       postPic:
-        "https://images.pexels.com/photos/1842182/pexels-photo-1842182.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
+        "https://cdn.pixabay.com/photo/2017/11/25/21/34/teddy-2977694_960_720.jpg",
       text: "Teddy was just enjoying the sun",
-      date: "March 1",
+      date: "March 1 2022 at 3:45pm",
       comments: [3],
       likes: 2,
       likers: [1, 2],
@@ -75,7 +75,7 @@ export const PostsFeed = ({ LikeUpdate, CommentUpdate }) => {
       postPic:
         "https://cdn.pixabay.com/photo/2020/12/15/14/38/covid-19-5833844_960_720.png",
       text: "Booster shots now available across Canada",
-      date: "Jan 10",
+      date: "Jan 10 2022 at 1:35pm",
       comments: [],
       likes: 2,
       likers: [1, 2],
@@ -92,7 +92,7 @@ export const PostsFeed = ({ LikeUpdate, CommentUpdate }) => {
         "https://images.pexels.com/photos/463734/pexels-photo-463734.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
       text: "Enjoying some quite time at the cabin",
       comments: [1, 5],
-      date: "March 2",
+      date: "March 2 2022 at 7:20am",
       likes: 2,
       likers: [2, 5],
       datePosted: new Date("2022-03-02"),
@@ -106,7 +106,7 @@ export const PostsFeed = ({ LikeUpdate, CommentUpdate }) => {
       postPic:
         "https://cdn.pixabay.com/photo/2019/11/10/11/13/couple-4615557_960_720.jpg",
       text: "Happy Anniversary Babe",
-      date: "Feb 22",
+      date: "Feb 22 2022 at 10:25pm",
       comments: [2, 4],
       likes: 4,
       likers: [1, 2, 3, 4],
@@ -121,68 +121,37 @@ export const PostsFeed = ({ LikeUpdate, CommentUpdate }) => {
       <div className="Post-Feed">
         {Posts.map((Post) => (
           <>
-            <div key={Post && Post.id} className="Posts">
-              <div className=" Poster">
-                <div className="PosterInfo">
-                  <img
-                    src={Post && Post.PosterPic}
-                    alt=""
-                    className="PostFeed-img "
-                  />
-                  {Post.PosterName}
-                </div>
-
-                <Link to="/Posts" style={{ color: "black" }}>
-                  {Post.text}
-                </Link>
-              </div>
-              <Link style={{ color: "black" }} to="/Posts">
-                {Post && Post.postPic !== "" ? (
-                  <img src={Post.postPic} alt="" className="PostPic" />
-                ) : (
-                  ""
-                )}
-              </Link>
-
-              <div className="Interactions" style={{ color: "black" }}>
-                <div>{Post && Post.date}</div>
-                <div>
-                  {Post.likers.length}{" "}
-                  <i className="fas fa-heart" style={{ color: "red" }}></i>
-                </div>
-
-                <div>
-                  {" "}
-                  {Post.comments.length}
-                  <i className="fas fa-comment" style={{ color: "grey" }}></i>
+            <div className="postFeed-post" key={Post.id}>
+              <div className="postFeed-post-info">
+                <img src={Post.PosterPic} alt="" />
+                <div className="postFeed-post-info-stamp">
+                  <h3>{Post.PosterName}</h3>
+                  <h5>Published {Post.date}</h5>
                 </div>
               </div>
-              <div className="Create-Post">
+              <div className="postFeed-post-image">
                 {" "}
-                <img src={user.profilePic} alt="" className="PostFeed-img " />
-                <input
-                  type="textarea"
-                  name="comment"
-                  className="PostComment"
-                  value={postID === Post.id ? formComment : ""}
-                />
-                <div></div>
-                {formComment.length > 0 && postID === Post.id ? (
-                  <div className="SubmitComment">
-                    <button
-                      className="SCButton"
-                      onClick={() => {
-                        CommentUpdate(formComment, user, Post.id);
-                        setFormComment("");
-                      }}
-                    >
-                      Post
-                    </button>
-                  </div>
-                ) : (
-                  ""
-                )}
+                <img src={Post.postPic} alt="" />
               </div>
+              <div className="postFeed-post-stats">
+                <div>
+                  <i class="fas fa-eye " style={{ color: "teal" }}></i>
+                  <small>12</small>
+                </div>
+                <div>
+                  <i class="far fa-heart" style={{ color: "crimson" }}></i>
+                  <small>{Post.likers.length}</small>
+                </div>
+                <div>
+                  <i class="fas fa-comment" style={{ color: "steelblue" }}></i>
+                  <small>{Post.comments.length}</small>
+                </div>
+                <div>
+                  <i class="fas fa-share" style={{ color: "navy" }}></i>
+                  <small>8</small>
+                </div>
+              </div>
+              <div className="postFeed-post-text">{Post.text}</div>
             </div>
           </>
         ))}
