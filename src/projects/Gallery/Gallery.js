6c3b7382474cd598b973e157dcc7443e9d26682b
moedeera <82620264+moedeera@users.noreply.github.com>
2022-04-03 05:29:59 -0600

    committ

--- src/projects/Gallery/Gallery.js
+++ src/projects/Gallery/Gallery.js
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Gallery.css";
 import Title from "./comps/Title";
 import { Uploadform } from "./comps/Uploadform";
 import { ImageGrid } from "./comps/ImageGrid";
+import Modal from "./comps/Modal";
 
 export const Gallery = () => {
+  const [selectedImg, setSelectedImg] = useState(null);
+
   return (
     <div className="Gallery">
       <Title />
       <Uploadform />
-      <ImageGrid />
+      <ImageGrid setSelectedImg={setSelectedImg} />
+      {selectedImg && (
+        <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg} />
+      )}
     </div>
   );
 };
