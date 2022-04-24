7b70fa9ba9cbe504d7f072d8a79e56ba73bad703
moedeera <82620264+moedeera@users.noreply.github.com>
2022-04-23 22:25:19 -0600

    committ

--- src/projects/socialmedia/login/Login.js
+++ src/projects/socialmedia/login/Login.js
@@ -1,5 +1,29 @@
 import React from "react";
+import "./Login.css";
 
 export const Login = () => {
-  return <div className="content">Logged out Content</div>;
+  return (
+    <div className="content login">
+      <div className="main-login">
+        <div className="main-login-form">
+          <h1>Login</h1>
+          <div>
+            <h3>Email</h3>
+            <input type="email" placeholder="Enter email" />
+          </div>
+
+          <div>
+            {" "}
+            <h3>Password</h3>
+            <input type="password" placeholder="Enter password" />
+          </div>
+
+          <button type="submit">Login</button>
+        </div>
+        <div className="main-login-picture">
+          <i class="fab fa-pied-piper"></i>
+        </div>
+      </div>
+    </div>
+  );
 };
