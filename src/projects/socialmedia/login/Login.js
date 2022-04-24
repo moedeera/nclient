09ca3d4195038252de46735244d2417f32fbb099
moedeera <82620264+moedeera@users.noreply.github.com>
2022-04-24 03:29:31 -0600

    committ

--- src/projects/socialmedia/login/Login.js
+++ src/projects/socialmedia/login/Login.js
@@ -1,14 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Login.css";
 import axios from "axios";
 
 export const Login = () => {
-  try {
-    var res = axios.get("http://localhost:5000");
-    console.log(res);
-  } catch (error) {
-    console.log(error);
-  }
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const { email, password } = formData;
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async (email, password) => {
+    console.log(email, password);
+
+    try {
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+      const body = JSON.stringify(formData);
+      const res = await axios.post(
+        "http://localhost:5000/api/users/login",
+        body,
+        config
+      );
+      console.log(res.data);
+      //save the token
+      // redirect to main page
+    } catch (error) {}
+  };
+
+  const fetchData = async () => {
+    try {
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+
+      const res = await axios.get("http://localhost:5000");
+      console.log(res.data);
+
+      // if (res.data){}
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <div className="login">
@@ -17,16 +60,35 @@ export const Login = () => {
           <h1>Login</h1>
           <div>
             <h3>Email</h3>
-            <input type="email" placeholder="Enter email" />
+            <input
+              type="email"
+              placeholder="Enter email"
+              name="email"
+              value={email}
+              onChange={(e) => onChange(e)}
+            />
           </div>
 
           <div>
             {" "}
             <h3>Password</h3>
-            <input type="password" placeholder="Enter password" />
+            <input
+              type="password"
+              placeholder="Enter password"
+              name="password"
+              value={password}
+              onChange={(e) => onChange(e)}
+            />
           </div>
 
-          <button type="submit">Login</button>
+          <button
+            type="submit"
+            onClick={() => {
+              onSubmit(email, password);
+            }}
+          >
+            Login
+          </button>
         </div>
         <div className="main-login-picture">
           <i className="fab fa-pied-piper-alt"></i>
