e582a215977333f463fbeb9598fc26d1e6fea9e4
moedeera <82620264+moedeera@users.noreply.github.com>
2022-04-29 20:43:27 -0600

    committ

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./portfolio.css";
 import social from "./SocialPic1.svg";
 import diner from "./diner.svg";
@@ -7,173 +7,105 @@ import elearning from "./Elearner.svg";
 import old from "./old.svg";
 
 export const Portfolio = () => {
+  const [hover, setHover] = useState(false);
+
+  const data = [
+    {
+      id: 1,
+      img: social,
+      header: "Social Media Network",
+      text: "A mock full stack social media website designed using ReactJSand a Firebase backend",
+      repo: "https://github.com/moedeera/nclient/tree/main/src/projects/socialmedia",
+      live: "/socialmedia",
+      color: "#003B36",
+    },
+    {
+      id: 2,
+      img: bootcamp,
+      header: "Coding Bootcamp site",
+      text: "  A mock coding Boot camp website built with CSS, HTML and JavaScript.It also can connect to a backend for process registration.",
+      live: "https://deercoded.com",
+      repo: "https://github.com/moedeera/FED-HT-website-design",
+      color: "#9A031E",
+    },
+    {
+      id: 3,
+      img: diner,
+      header: "Dining site",
+      text: "  A dining website for a local business in Saskatoon, This was created using CSS,JavaScript and googleMaps.",
+      live: "https://super-donair.com",
+      repo: "https://github.com/moedeera/SuperDoniar",
+      color: "steelblue",
+    },
+    {
+      id: 4,
+      img: elearning,
+      header: "E-learning website",
+      text: " A full stack E-learning website built using JavaScript, Node and Express.",
+      live: "https://www.deeracode.com/Projects/profile",
+      repo: "https://github.com/moedeera/Login-Practice",
+      color: "#0E6BA8",
+    },
+    {
+      id: 5,
+      img: old,
+      header: "Old Portfolio Website",
+      text: "This was started around 2020 and was my first attempt to create a portfolio website.",
+      live: "https://www.deeracode.com",
+      repo: "https://github.com/moedeera/Login-Practice",
+      color: "#E98A15",
+    },
+    {
+      id: 6,
+      img: old,
+      header: "Old Portfolio Website",
+      text: "This was started around 2020 and was my first attempt to create a portfolio website.",
+      live: "www.deeracode.com",
+      repo: "https://github.com/moedeera/Login-Practice",
+      color: "#5448C8",
+    },
+  ];
+  console.log(data);
   return (
     <section id="portfolio" className="container">
       <h2>Portfolio</h2>
+
       <div className="portfolio_container">
-        <article className="portfolio_item social-media-project">
-          <img src={social} alt="" className="portfolio_image" />
-          <div className="portfolio_item_image ">
-            <div className="portfolio_info">
-              <h3>Social Media Network</h3>
-              <p>
-                A mock full stack social media website designed using ReactJS
-                and a Firebase backend.
-              </p>
-            </div>
+        {data.map((project) => (
+          <article
+            key={project.id}
+            className="portfolio_item social-media-project"
+            style={{
+              backgroundColor: project.color,
+              "&:hover": {
+                backgroundColor: "black",
+              },
+            }}
+          >
+            <img src={project.img} alt="" className="portfolio_image" />
+            <div className="portfolio_item_image ">
+              <div className="portfolio_info">
+                <h3>{project.header}</h3>
+                <p>{project.text}</p>
+              </div>
 
-            <div className="portfolio_links">
-              <a
-                href="https://github.com/moedeera/nclient/tree/main/src/projects/socialmedia"
-                className="btn spacing"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Repository
-                <i class="fa fa-github" aria-hidden="true"></i>
-              </a>
-              <a href="/socialmedia" className="btn btn-primary">
-                Live Demo
-              </a>
+              <div className="portfolio_links">
+                <a
+                  href={project.repo}
+                  className="btn spacing"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Repository
+                  <i className="fa fa-github" aria-hidden="true"></i>
+                </a>
+                <a href={project.live} className="btn btn-primary">
+                  Live Demo
+                </a>
+              </div>
             </div>
-          </div>
-        </article>
-
-        <article className="portfolio_item">
-          <img src={bootcamp} alt="" className="portfolio_image " />
-          <div className="portfolio_item_image">
-            <div className="portfolio_info">
-              {" "}
-              <h3>Coding Bootcamp site</h3>
-              <p>
-                A mock coding Boot camp website built with CSS, HTML and
-                JavaScript.It also can connect to a backend for process
-                registration.
-              </p>
-            </div>
-            <div className="portfolio_links">
-              <a
-                href="https://github.com/moedeera/FED-HT-website-design"
-                className="btn spacing"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Repository
-                <i class="fa fa-github" aria-hidden="true"></i>
-              </a>
-              <a href="https://deercoded.com" className="btn btn-primary">
-                Live Demo
-              </a>
-            </div>
-          </div>
-        </article>
-
-        <article className="portfolio_item">
-          <img src={diner} alt="" className="portfolio_image" />
-          <div className="portfolio_item_image">
-            <div className="portfolio_info">
-              {" "}
-              <h3>Dining site</h3>
-              <p>
-                A dining website for a local business in Saskatoon, This was
-                created using CSS,JavaScript and googleMaps.
-              </p>
-            </div>
-
-            <div className="portfolio_links">
-              <a
-                href="https://github.com"
-                className="btn spacing"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Repository
-                <i class="fa fa-github" aria-hidden="true"></i>
-              </a>
-              <a href="https://super-donair.com" className="btn btn-primary">
-                Live Demo
-              </a>
-            </div>
-          </div>
-        </article>
-
-        <article className="portfolio_item">
-          <img src={elearning} alt="" className="portfolio_image" />
-          <div className="portfolio_item_image">
-            <div className="portfolio_info">
-              {" "}
-              <h3>Elearning website</h3>
-              <p>
-                A full stack Elearning website built using JavaScript, Node and
-                Express.
-              </p>
-            </div>
-            <div className="portfolio_links">
-              <a
-                href="https://github.com"
-                className="btn spacing"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Repository
-                <i class="fa fa-github" aria-hidden="true"></i>
-              </a>
-              <a
-                href="https://www.deeracode.com/Projects/profile"
-                className="btn btn-primary"
-              >
-                Live Demo
-              </a>
-            </div>
-          </div>
-        </article>
-
-        <article className="portfolio_item">
-          <img src={old} alt="" />
-          <div className="portfolio_item_image">
-            <div className="portfolio_info">
-              <h3>Old Portfolio Website</h3>
-              <p>
-                This was started around 2020 and was my first attempt to create
-                a portfolio website.
-              </p>
-            </div>
-            <div className="portfolio_links">
-              <a
-                href="https://github.com"
-                className="btn"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Repository
-                <i class="fa fa-github" aria-hidden="true"></i>
-              </a>
-              <a href="https://github.com" className="btn btn-primary">
-                Live Demo
-              </a>
-            </div>
-          </div>
-        </article>
-        <article className="portfolio_item">
-          <img src={social} alt="" />
-          <div className="portfolio_item_image">
-            <h3>Elearning Website</h3>
-            <div className="portfolio_links">
-              <a
-                href="https://github.com"
-                className="btn"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Repository
-                <i class="fa fa-github" aria-hidden="true"></i>
-              </a>
-              <a href="https://github.com" className="btn btn-primary">
-                Live Demo
-              </a>
-            </div>
-          </div>
-        </article>
+          </article>
+        ))}
       </div>
     </section>
   );
