6ea608790a005cc41ee1e59449b5490256e1f40b
moedeera <82620264+moedeera@users.noreply.github.com>
2022-05-02 08:20:49 -0600

    committ

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,14 +6,29 @@ export const About = () => {
     <section id="about">
       <h1>About Me</h1>
       <p>
-        I am self-taught developer since 2020, I have built many projects and
-        learned multiple skills during that time-frame. The coding industry
-        appeals to me because it always encourages learning and keeping up to
-        date with new trends and technologies. I truly enjoy discovering new
-        languages and building new projects. If you are interested in partnering
-        up or for hiring purposes, just send me a message and I will respond as
-        soon as possible.
+        I am self-taught web developer who started learning code in 2020. I have
+        built many projects and learned multiple skills during that time-frame.
+        The coding industry appeals to me because it always encourages us to
+        keep learning and staying up to date with new trends and technologies. I
+        truly enjoy discovering new languages and building new projects. If you
+        are interested in a partnership or hiring purposes, just send me a
+        message and I will respond as soon as possible.
       </p>
+      <h1>Skills</h1>
+      <ul>
+        <li>
+          <i className="fab fa-css3-alt fa-3x"></i>
+        </li>
+        <li>
+          <i className="fab fa-js fa-3x"></i>
+        </li>
+        <li>
+          <i className="fab fa-react fa-3x"></i>
+        </li>
+        <li>
+          <i className="fab fa-node fa-3x"></i>
+        </li>
+      </ul>
     </section>
   );
 };
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -7,10 +7,14 @@ export const Contact = () => {
       <h1>Get in Touch</h1>
       <form action="https://formsubmit.co/deeracode@gmail.com" method="POST">
         <label>Email</label>
-        <input type="email" name="email" />
+        <input placeholder="Enter your email " type="email" name="email" />
 
         <label>Message</label>
-        <textarea id="message" name="message" />
+        <textarea
+          placeholder="Enter a detailed message "
+          id="message"
+          name="message"
+        />
         <button className="btn btn-secondary" type="submit">
           Send
         </button>
